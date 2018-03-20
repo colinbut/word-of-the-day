8a7f6d4c9cb84baad86dde46e6840ce3ce1a4fe7
Colin But <colinbut@users.noreply.github.com>
2018-03-20 21:55:57 +0000

    reformat code

--- src/main/java/com/mycompany/wotd/wotd/WotdApplication.java
+++ src/main/java/com/mycompany/wotd/wotd/WotdApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WotdApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WotdApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WotdApplication.class, args);
+    }
 }
