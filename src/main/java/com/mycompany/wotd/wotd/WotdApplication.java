be4bc3d0008ed09307caa7a52bb4689e55801bb9
Colin But <colinbut@users.noreply.github.com>
2018-03-21 18:26:42 +0000

    app

--- src/main/java/com/mycompany/wotd/wotd/WotdApplication.java
+++ src/main/java/com/mycompany/wotd/wotd/WotdApplication.java
@@ -2,11 +2,21 @@ package com.mycompany.wotd.wotd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class WotdApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WotdApplication.class, args);
+    }
+
+    @RequestMapping(value = "/word", method = RequestMethod.GET)
+    public ResponseEntity<String> getWordOfTheDay(){
+        return ResponseEntity.ok("Brilliant");
     }
 }
